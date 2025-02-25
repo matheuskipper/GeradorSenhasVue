2a39a3dc9efde2f0a0343984ce84f7d74161b61a
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-25 12:20:43 -0300

    inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+import generator from "generate-password-browser";
+
+let password = generator.generate({
+  length: 10,
+  numbers: true,
+});
+
+console.log(password);
+</script>
+
+<style scoped></style>
