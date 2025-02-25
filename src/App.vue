4ade8b8b267daccdc1de825e0983854aa9bfffc8
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-25 13:35:57 -0300

    Adicionando Tailwind CSS ao projeto

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="font-bold">Generate Password</h1>
+  </div>
+</template>
 
 <script>
 import generator from "generate-password-browser";
