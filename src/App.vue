1d9377efceb3271e738859afcd8a04757cede7b0
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-26 08:20:34 -0300

    Melhorando a estilização com Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -3,63 +3,71 @@
     class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4"
   >
     <h1 class="font-bold text-[35px] p-4">Generate Password</h1>
-    <div class="p-4">
-      <label for="password-length">Password Size: {{ length }}</label>
-      <input
-        type="range"
-        min="8"
-        max="20"
-        v-model="length"
-        class="w-full mt-2 cursor-pointer"
-      />
-    </div>
-    <div class="flex flex-col gap-2 mb-3 p-2">
-      <label class="cursor-pointer flex items-center" for="input-uppercase"
-        ><input
-          id="input-uppercase"
-          class="cursor-pointer mr-2 w-5 h-5"
-          type="checkbox"
-          v-model="includeUppercase"
-        />Include uppercase</label
-      >
-      <label class="cursor-pointer flex items-center" for="input-numbers"
-        ><input
-          id="input-numbers"
-          class="cursor-pointer mr-2 w-5 h-5"
-          type="checkbox"
-          v-model="includeNumbers"
-        />Include numbers</label
-      >
-      <label class="cursor-pointer flex items-center" for="input-symbols"
-        ><input
-          id="input-symbols"
-          class="cursor-pointer mr-2 w-5 h-5"
-          type="checkbox"
-          v-model="includeSymbols"
-        />Include symbols</label
-      >
-    </div>
-    <button
-      @click="generatePassword"
-      class="bg-blue-500 hover:bg-blue-600 font-bold text-white px-4 py-2 rounded-lg cursor-pointer"
-    >
-      Generate
-    </button>
-
     <div
-      v-if="password"
-      class="mt-4 p-4 border border-yellow-500 bg-yellow-100 text-yellow-900 rounded-lg animate-fade-in"
+      class="w-full max-w-md p-6 bg-white rounded-lg shadow-lg text-gray-900"
     >
-      <h1 class="text-lg font-bold tracking-wide">{{ password }}</h1>
-    </div>
+      <div class="p-4">
+        <label for="password-length" class="block text-lg font-medium mb-2"
+          >Password Size: {{ length }}</label
+        >
+        <input
+          type="range"
+          min="8"
+          max="20"
+          v-model="length"
+          class="w-full mt-2 cursor-pointer"
+        />
+      </div>
+      <div class="flex flex-col gap-4 mb-6">
+        <label class="cursor-pointer flex items-center" for="input-uppercase"
+          ><input
+            id="input-uppercase"
+            class="cursor-pointer mr-2 w-6 h-6"
+            type="checkbox"
+            v-model="includeUppercase"
+          />Include uppercase</label
+        >
+        <label class="cursor-pointer flex items-center" for="input-numbers"
+          ><input
+            id="input-numbers"
+            class="cursor-pointer mr-2 w-6 h-6"
+            type="checkbox"
+            v-model="includeNumbers"
+          />Include numbers</label
+        >
+        <label class="cursor-pointer flex items-center" for="input-symbols"
+          ><input
+            id="input-symbols"
+            class="cursor-pointer mr-2 w-6 h-6"
+            type="checkbox"
+            v-model="includeSymbols"
+          />Include symbols</label
+        >
+      </div>
+      <button
+        @click="generatePassword"
+        class="bg-blue-500 hover:bg-blue-600 font-bold text-white px-4 py-3 rounded-lg shadow-md transition duration-300 cursor-pointer"
+      >
+        Generate
+      </button>
 
-    <button
-      v-if="password"
-      @click="copyPassword"
-      class="bg-yellow-500 hover:bg-yellow-600 font-bold text-white px-4 py-2 rounded-lg cursor-pointer mt-4"
-    >
-      Copy
-    </button>
+      <div
+        v-if="password"
+        class="flex items-center justify-between mt-6 p-4 border border-yellow-500 bg-yellow-100 text-yellow-900 rounded-lg animate-fade-in"
+      >
+        <h1 class="text-lg font-bold tracking-wide">
+          {{ password }}
+        </h1>
+
+        <button
+          v-if="password"
+          @click="copyPassword"
+          class="bg-yellow-500 hover:bg-yellow-600 font-bold text-white px-4 py-2 rounded-lg cursor-pointer"
+        >
+          Copy
+        </button>
+      </div>
+    </div>
   </div>
   <footer class="bg-gray-600 text-white p-3">
     <p class="text-center text-sm">
